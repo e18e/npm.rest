c9bb8680f6b96d47de8c0ae448acd87fda9df897
Willow (GHOST) <git@willow.sh>
2026-02-04 02:45:48 +0000

    feat: more data in state endpoint

--- packages/api/src/routes/v0/state.get.ts
+++ packages/api/src/routes/v0/state.get.ts
@@ -1,9 +1,15 @@
-import { changeTable } from '@npm.rest/db/schema';
 import { defineHandler } from 'nitro/h3';
 import { env } from 'cloudflare:workers';
 import { defineRouteMeta } from 'nitro';
 import { count } from 'drizzle-orm';
 import { getDB } from '$lib/db';
+import {
+	dependencyTable,
+	packageTable,
+	versionTable,
+	changeTable,
+	specifierTable,
+} from '@npm.rest/db/schema';
 
 defineRouteMeta({
 	openAPI: {
@@ -14,7 +20,7 @@ defineRouteMeta({
 export default defineHandler(async (event) => {
 	const db = await getDB(env);
 
-	const changeCounts = await db
+	const changes = await db
 		.select({
 			state: changeTable.state,
 			count: count(),
@@ -22,10 +28,17 @@ export default defineHandler(async (event) => {
 		.from(changeTable)
 		.groupBy(changeTable.state);
 
+	const packages = await db.$count(packageTable);
+	const versions = await db.$count(versionTable);
+	const dependencies = await db.$count(dependencyTable);
+	const specifiers = await db.$count(specifierTable);
+
 	return {
 		success: true,
-		changes: Object.fromEntries(
-			changeCounts.map((c) => [c.state, c.count]),
-		),
+		changes: Object.fromEntries(changes.map((c) => [c.state, c.count])),
+		packages,
+		versions,
+		dependencies,
+		specifiers,
 	};
 });
