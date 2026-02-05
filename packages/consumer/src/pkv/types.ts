c3b6aba1a2f25d63cd91202f55fad674bc5ec956
Willow (GHOST) <git@willow.sh>
2026-02-05 14:11:15 +0000

    perf(consumer): speed up dt package check

--- packages/consumer/src/pkv/types.ts
+++ packages/consumer/src/pkv/types.ts
@@ -1,14 +1,14 @@
 import { processPackument } from '../shared/packument';
+import { packageTable } from '@npm.rest/db/schema';
 import type { UnpackResult } from '@publint/pack';
+import { db } from '@npm.rest/db/server';
 import { Result } from 'better-result';
 import { LRUCache } from 'lru-cache';
 import { extname } from 'node:path';
+import { eq } from 'drizzle-orm';
 
 const TS_FILE_EXTENSIONS = ['.ts', '.cts', '.mts', '.tsx'];
 
-const dtCache = new LRUCache<string, boolean>({ max: 1000 });
-
-// todo https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/161725ad2e8957f109e44fb26b13c9d70f415c2f/packages/core/src/createPackage.ts#L186-L200
 export async function hasTypes(name: string, tarball: UnpackResult) {
 	if (name.startsWith('@types/')) {
 		return Result.ok('built-in' as const);
@@ -22,29 +22,49 @@ export async function hasTypes(name: string, tarball: UnpackResult) {
 		return Result.ok('built-in' as const);
 	}
 
-	if (dtCache.has(name)) {
-		return Result.ok(
-			dtCache.get(name)
-				? ('definitely-typed' as const)
-				: ('none' as const),
-		);
+	const typesPkgName = name.startsWith('@')
+		? name.replace('/', '__').replace('@', '@types/')
+		: `@types/${name}`;
+
+	const hasTypesPkg = await typesPackageExists(typesPkgName);
+	if (hasTypesPkg.isErr()) return hasTypesPkg;
+
+	return Result.ok(
+		hasTypesPkg ? ('definitely-typed' as const) : ('none' as const),
+	);
+}
+
+const typesPackageCache = new LRUCache<string, boolean>({ max: 1000 });
+
+// todo https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/161725ad2e8957f109e44fb26b13c9d70f415c2f/packages/core/src/createPackage.ts#L186-L200
+async function typesPackageExists(typesPkgName: string) {
+	if (typesPackageCache.has(typesPkgName)) {
+		return Result.ok(true);
 	}
 
-	const pkg = await processPackument(
-		name.startsWith('@')
-			? name.replace('/', '__').replace('@', '@types/')
-			: `@types/${name}`,
-	);
+	const pkgExists = await db
+		.select({ id: packageTable.id })
+		.from(packageTable)
+		.where(eq(packageTable.name, typesPkgName))
+		.then((records) => records.length > 0);
 
-	if (pkg.isErr()) {
-		if ('status' in pkg.error && pkg.error.status === 404) {
-			dtCache.set(name, false);
-			return Result.ok('none' as const);
+	if (pkgExists) {
+		typesPackageCache.set(typesPkgName, true);
+		return Result.ok(true);
+	}
+
+	const packument = await processPackument(typesPkgName);
+
+	if (packument.isErr()) {
+		if ('status' in packument.error && packument.error.status === 404) {
+			typesPackageCache.set(typesPkgName, false);
+			return Result.ok(false);
 		}
 
-		return pkg;
+		typesPackageCache.delete(typesPkgName);
+		return packument;
 	}
 
-	dtCache.set(name, true);
-	return Result.ok('definitely-typed' as const);
+	typesPackageCache.set(typesPkgName, true);
+	return Result.ok(true);
 }
