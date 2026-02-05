988ab1cb65d0465bd463f258df2a55af8b3cd30d
Willow (GHOST) <git@willow.sh>
2026-02-05 22:30:40 +0000

    chore: enable test

--- packages/consumer/test/pkv/types.test.ts
+++ packages/consumer/test/pkv/types.test.ts
@@ -16,6 +16,8 @@ const TS_UNPACK: UnpackResult = {
 	rootDir: '/',
 };
 
+const REV = '1-placeholder';
+
 vi.mock('@npm.rest/db/server', async () => {
 	const { drizzle } = await import('drizzle-orm/postgres-js');
 
@@ -34,13 +36,13 @@ vi.mock(import('../../src/shared/logger'), async () => {
 
 describe('hasTypes()', () => {
 	it('returns built-in if a types package is given', async () => {
-		const types = await hasTypes('@types/semver', EMPTY_UNPACK);
+		const types = await hasTypes('@types/semver', EMPTY_UNPACK, REV);
 		expect(types.status).toBe('ok');
 		expect(types.unwrap()).toBe('built-in');
 	});
 
 	it('returns built-in when a types package has a ts file', async () => {
-		const types = await hasTypes('semver', TS_UNPACK);
+		const types = await hasTypes('semver', TS_UNPACK, REV);
 		expect(types.status).toBe('ok');
 		expect(types.unwrap()).toBe('built-in');
 	});
@@ -56,36 +58,37 @@ describe('hasTypes()', () => {
 			npmUpdatedAt: new Date(),
 		});
 
-		const types = await hasTypes(name, EMPTY_UNPACK);
+		const types = await hasTypes(name, EMPTY_UNPACK, REV);
 		expect(types.status).toBe('ok');
 		expect(types.unwrap()).toBe('definitely-typed');
 	});
 
-	// it('returns definitely-typed when a types package exists as a packument', async () => {
-	// 	const name = `:${crypto.randomUUID()}`;
+	it('returns definitely-typed when a types package exists as a packument', async () => {
+		const name = `:${crypto.randomUUID()}`;
 
-	// 	await db.insert(packumentTable).values({
-	// 		id: `@types/${name}`,
-	// 		data: {
-	// 			name: `@types/${name}`,
-	// 			time: {
-	// 				created: new Date().toISOString(),
-	// 				modified: new Date().toISOString(),
-	// 			},
-	// 		},
-	// 	});
+		await db.insert(packumentTable).values({
+			id: `@types/${name}`,
+			revId: '1-placeholder',
+			data: {
+				name: `@types/${name}`,
+				time: {
+					created: new Date().toISOString(),
+					modified: new Date().toISOString(),
+				},
+			},
+		});
 
-	// 	vi.stubGlobal(
-	// 		'fetch',
-	// 		vi
-	// 			.fn()
-	// 			.mockRejectedValue(
-	// 				new Error("fetch shouldn't have been called"),
-	// 			),
-	// 	);
+		vi.stubGlobal(
+			'fetch',
+			vi
+				.fn()
+				.mockRejectedValue(
+					new Error("fetch shouldn't have been called"),
+				),
+		);
 
-	// 	const types = await hasTypes(name, EMPTY_UNPACK);
-	// 	expect(types.status).toBe('ok');
-	// 	expect(types.unwrap()).toBe('definitely-typed');
-	// });
+		const types = await hasTypes(name, EMPTY_UNPACK, '1-placeholder');
+		expect(types.status).toBe('ok');
+		expect(types.unwrap()).toBe('definitely-typed');
+	});
 });
